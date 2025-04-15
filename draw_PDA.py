9ece8df5095b0479766edb1546b1769bce019a43
MattiaOldani <mattiaoldani6@gmail.com>
2025-04-15 09:13:59 +0200

    Aggiunto package per i PDA

---
+++ draw_PDA.py
@@ -0,0 +1,54 @@
+from automata.pda.dpda import DPDA
+from automata.pda.npda import NPDA
+
+
+dpda = DPDA(
+    states={""},
+    input_symbols={""},
+    stack_symbols={""},
+    transitions={
+        "": {
+            "": {
+                "": ("", ("")),
+                "": ("", ("")),
+            },
+            "": {
+                "": ("", ("")),
+                "": ("", ("")),
+            },
+        },
+    },
+    initial_state="",
+    initial_stack_symbol="",
+    final_states={""},
+    # empty_stack, final_states, both
+    acceptance_mode="",
+)
+
+dpda.show_diagram(path="...")
+
+
+npda = NPDA(
+    states={""},
+    input_symbols={""},
+    stack_symbols={""},
+    transitions={
+        "": {
+            "": {
+                "": {("", (""))},
+                "": {("", (""))},
+            },
+            "": {
+                "": {("", (""))},
+                "": {("", (""))},
+            },
+        },
+    },
+    initial_state="",
+    initial_stack_symbol="",
+    final_states={""},
+    # empty_stack, final_states, both
+    acceptance_mode="",
+)
+
+npda.show_diagram(path="...")
